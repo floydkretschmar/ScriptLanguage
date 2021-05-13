5e77b82dc28eaa0cab17adab991614ed61446464
Floyd Kretschmar <floyd.kretschmar@student.uni-tuebingen.de>
2021-05-13 17:02:01 +0200

    Fixed unary operators

--- FlKr.ScriptLanguage/Parsing/Parser.LogicOperation.cs
+++ FlKr.ScriptLanguage/Parsing/Parser.LogicOperation.cs
@@ -20,13 +20,13 @@ namespace FlKr.ScriptLanguage.Parsing
                 var orExpression = ParseAndOperationExpression(orConditions.First(), out var subDataType);
                 if (subDataType != typeof(bool))
                     throw new ParseException(orConditions.First(), "Expression is not a valid boolean expression.");
-                        
+
                 for (int i = 1; i < orConditions.Count; i++)
                 {
                     var otherOrExpression = ParseAndOperationExpression(orConditions[i], out subDataType);
                     if (subDataType != typeof(bool))
                         throw new ParseException(orConditions[i], "Expression is not a valid boolean expression.");
-                    
+
                     orExpression = Expression.OrElse(orExpression, otherOrExpression);
                 }
 
@@ -38,7 +38,7 @@ namespace FlKr.ScriptLanguage.Parsing
                 return ParseAndOperationExpression(expression, out dataType);
             }
         }
-        
+
         private Expression ParseAndOperationExpression(List<IToken> expression, out Type dataType)
         {
             List<List<IToken>> andConditions = SplitIntoExpressions(
@@ -48,16 +48,16 @@ namespace FlKr.ScriptLanguage.Parsing
 
             if (andConditions.Count > 1)
             {
-                Expression andExpression = ParseEqualsOperationExpression(andConditions.First(), out var subDataType);
+                Expression andExpression = ParseNotOperationExpression(andConditions.First(), out var subDataType);
                 if (subDataType != typeof(bool))
                     throw new ParseException(andConditions.First(), "Expression is not a valid boolean expression.");
-                
+
                 for (int i = 1; i < andConditions.Count; i++)
                 {
-                    var otherAndExpression = ParseEqualsOperationExpression(andConditions[i], out subDataType);
+                    var otherAndExpression = ParseNotOperationExpression(andConditions[i], out subDataType);
                     if (subDataType != typeof(bool))
                         throw new ParseException(andConditions[i], "Expression is not a valid boolean expression.");
-                    
+
                     andExpression = Expression.AndAlso(andExpression, otherAndExpression);
                 }
 
@@ -66,25 +66,58 @@ namespace FlKr.ScriptLanguage.Parsing
             }
             else
             {
-                return ParseEqualsOperationExpression(expression, out dataType);
+                return ParseNotOperationExpression(expression, out dataType);
             }
         }
-        
+
+        private Expression ParseNotOperationExpression(List<IToken> expression, out Type dataType)
+        {
+            if (expression.First().DetailType == TokenDetailTypes.Not)
+            {
+                if (expression.Count < 2)
+                    throw new ParseException(expression, "Invalid negation in expression");
+
+                return Expression.Not(
+                    ParseNotOperationExpression(
+                        expression.GetRange(1, expression.Count - 1), out dataType));
+            }
+
+            // Singular truth value or variable
+            if (expression.Count == 1)
+            {
+                var parsedValue = ParseValueExpression(expression[0], out dataType);
+                if (dataType != typeof(bool))
+                    throw new ParseException(expression,
+                        "Invalid negation: Negated value has to return boolean values.");
+                return parsedValue;
+            }
+            // equality expression
+            else
+            {
+                var expressionValue = ParseEqualsOperationExpression(expression, out dataType);
+                if (dataType != typeof(bool))
+                    throw new ParseException(expression,
+                        "Invalid negation: Negated expressions have to return boolean values.");
+
+                return expressionValue;
+            }
+        }
+
         private Expression ParseEqualsOperationExpression(List<IToken> expression, out Type dataType)
         {
             List<List<IToken>> equalsConditions = SplitIntoExpressions(
                 expression,
                 true,
                 TokenDetailTypes.Equals);
-            
-            if (equalsConditions.Count > 2)
+
+            if (equalsConditions.Count != 2)
             {
                 throw new ParseException(expression, "Invalid equality expression");
             }
-            else if (equalsConditions.Count == 2)
+            else
             {
-                var leftPart = ParseNotOperationExpression(equalsConditions[0], expression, out var leftSubType);
-                var rightPart = ParseNotOperationExpression(equalsConditions[1], expression, out var rightSubType);
+                var leftPart = ParseAdditionOperationExpression(equalsConditions[0], out var leftSubType);
+                var rightPart = ParseAdditionOperationExpression(equalsConditions[1], out var rightSubType);
 
                 if (leftSubType != rightSubType)
                     throw new ParseException(expression, "Data types on both sides of the equation do not align.");
@@ -92,27 +125,6 @@ namespace FlKr.ScriptLanguage.Parsing
                 dataType = typeof(bool);
                 return Expression.Equal(leftPart, rightPart);
             }
-            else
-            {
-                return ParseNotOperationExpression(expression, expression, out dataType);
-            }
-        }
-
-        private Expression ParseNotOperationExpression(List<IToken> expression, List<IToken> parentExpression,
-            out Type dataType)
-        {
-            if (expression.First().DetailType == TokenDetailTypes.Not)
-            {
-                if (expression.Count < 2)
-                    throw new ParseException(parentExpression, "Invalid negation in equality expression");
-
-                dataType = typeof(bool);
-                return Expression.Not(
-                    ParseAdditionOperationExpression(
-                        expression.GetRange(1, expression.Count - 1)));
-            }
-
-            return ParseAdditionOperationExpression(expression, out dataType);
         }
     }
 }
--- FlKr.ScriptLanguage/Parsing/Parser.MathOperation.cs
+++ FlKr.ScriptLanguage/Parsing/Parser.MathOperation.cs
@@ -14,14 +14,14 @@ namespace FlKr.ScriptLanguage.Parsing
         {
             return ParseAdditionOperationExpression(expression, out var dataType);
         }
-        
+
         private Expression ParseAdditionOperationExpression(List<IToken> expression, out Type dataType)
         {
             List<List<IToken>> parts = SplitIntoExpressions(
                 expression,
                 true,
                 TokenDetailTypes.Addition);
-        
+
             if (parts.Count > 1)
             {
                 return ParseMathOperationExpression(
@@ -30,7 +30,7 @@ namespace FlKr.ScriptLanguage.Parsing
                     ParseSubtractionOperationExpression,
                     out dataType);
             }
-        
+
             return ParseSubtractionOperationExpression(expression, out dataType);
         }
 
@@ -40,59 +40,76 @@ namespace FlKr.ScriptLanguage.Parsing
                 expression,
                 true,
                 TokenDetailTypes.Subtraction);
-        
+
             if (parts.Count > 1)
                 return ParseMathOperationExpression(
                     parts,
                     Expression.Subtract,
                     ParseMultiplicationOperationExpression,
                     out dataType);
-        
+
             return ParseMultiplicationOperationExpression(expression, out dataType);
         }
-        
+
         private Expression ParseMultiplicationOperationExpression(List<IToken> expression, out Type dataType)
         {
             List<List<IToken>> parts = SplitIntoExpressions(
                 expression,
                 true,
                 TokenDetailTypes.Multiplication);
-        
+
             if (parts.Count > 1)
                 return ParseMathOperationExpression(
                     parts,
                     Expression.Multiply,
                     ParseDivisionOperationExpression,
                     out dataType);
-            
+
             return ParseDivisionOperationExpression(expression, out dataType);
         }
-        
+
         private Expression ParseDivisionOperationExpression(List<IToken> expression, out Type dataType)
         {
             List<List<IToken>> parts = SplitIntoExpressions(
                 expression,
                 true,
                 TokenDetailTypes.Division);
-        
+
             if (parts.Count > 1)
                 return ParseMathOperationExpression(
                     parts,
                     Expression.Divide,
-                    ParseUnaryNegativeExpression,
+                    ParseNegativeExpression,
                     out dataType);
+
+            return ParseNegativeExpression(expression, out dataType);
+        }
+
+        private Expression ParseNegativeExpression(List<IToken> expression, out Type dataType)
+        {
+            if (expression.First().DetailType == TokenDetailTypes.Negative)
+            {
+                if (expression.Count < 2)
+                    throw new ParseException(expression, "Invalid unary negative expression");
+
+                return Expression.Negate(
+                    ParseNegativeExpression(
+                        expression.GetRange(1, expression.Count - 1), out dataType));
+            }
             
-            return ParseUnaryNegativeExpression(expression, out dataType);
+            if (expression.Count > 1)
+                throw new ParseException(expression, "Invalid negative value expression.");
+
+            var negatedExpression = ParseValueExpression(expression[0], out dataType);
+            if (dataType != typeof(double) && dataType != typeof(int))
+                throw new ParseException(expression,
+                    "Invalid negation: Negated expressions have to return numeric values.");
+            return negatedExpression;
         }
-        
-        private Expression ParseUnaryNegativeExpression(List<IToken> expression, out Type dataType)
+
+        private Expression ParseValueExpression(IToken valueToken, out Type dataType)
         {
-            throw new NotImplementedException();
-        }
-        
-        private Expression ParseValueExpression(IToken token, out Type dataType)
-        {
-            switch (token.DetailType)
+            switch (valueToken.DetailType)
             {
                 case TokenDetailTypes.True:
                     dataType = typeof(bool);
@@ -102,28 +119,29 @@ namespace FlKr.ScriptLanguage.Parsing
                     return Expression.Constant(false, dataType);
                 case TokenDetailTypes.FloatingPoint:
                     dataType = typeof(double);
-                    return Expression.Constant(double.Parse(((Token) token).Value), dataType);
+                    return Expression.Constant(double.Parse(((Token) valueToken).Value), dataType);
                 case TokenDetailTypes.Integer:
                     dataType = typeof(int);
-                    return Expression.Constant(int.Parse(((Token) token).Value), dataType);
+                    return Expression.Constant(int.Parse(((Token) valueToken).Value), dataType);
                 case TokenDetailTypes.Text:
                     dataType = typeof(string);
-                    return Expression.Constant(((Token) token).Value, dataType);
+                    return Expression.Constant(((Token) valueToken).Value, dataType);
                 case TokenDetailTypes.Expression:
-                    var expressionToken = (ExpressionToken)token;
+                    var expressionToken = (ExpressionToken) valueToken;
                     dataType = expressionToken.DataType;
                     return expressionToken.Value;
                 case TokenDetailTypes.VariableName:
-                    if (!_variables.TryGetValue(((Token) token).Value, out var variable))
-                        throw new ParseException(new List<IToken>() {token}, "Variable has not been declared.");
+                    if (!_variables.TryGetValue(((Token) valueToken).Value, out var variable))
+                        throw new ParseException(new List<IToken>() {valueToken}, "Variable has not been declared.");
                     dataType = variable.Type;
                     return variable;
                 default:
-                    throw new ParseException(new List<IToken>() {token}, $"The token type {token.DetailType} is not a valid value expression.");
+                    throw new ParseException(new List<IToken>() {valueToken},
+                        $"The token type {valueToken.DetailType} is not a valid value expression.");
             }
         }
-        
-        
+
+
         private Expression ParseMathOperationExpression(
             List<List<IToken>> expressions,
             Func<Expression, Expression, BinaryExpression> expressionDefinition,
@@ -133,21 +151,21 @@ namespace FlKr.ScriptLanguage.Parsing
             var expression = parseOperation(expressions.First(), out dataType);
             if (dataType != typeof(int) && dataType != typeof(double))
                 throw new ParseException(expressions.First(), "Expression is not a valid numerical expression.");
-        
+
             for (int i = 1; i < expressions.Count; i++)
             {
                 var otherExpression = parseOperation(expressions[i], out var subDataType);
                 if (subDataType != typeof(int) && subDataType != typeof(double))
                     throw new ParseException(expressions.First(),
                         "Expression is not a valid numerical expression.");
-        
+
                 // only use decimal point math when necessary
                 if (dataType == typeof(int) && subDataType == typeof(double))
                     dataType = typeof(double);
-        
+
                 expression = expressionDefinition(expression, otherExpression);
             }
-        
+
             return expression;
         }
     }
